283d2b6b9ac6a8e94e7c79600d405e9549f711e4
Youssef Guba <youssefguba6@gmail.com>
2026-03-03 22:59:02 +0200

    Typo issue

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many timesssssssss:'),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
